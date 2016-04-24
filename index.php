f53eda1976634227f6d080efac4b09650fbfb94e
BigTruGa10 <strujillog1401@alumno.ipn.mx>
2016-04-24 14:34:12 -0500

    Boton inicial

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
     <body style="background-color: deeppink;color: aqua">
         <h1>Chess pizza :D</h1>
         <h2>Prueba :D</h2>
+        <form action="getLocation.php" method="POST">
+        <input type="submit" value="Pedir pizza">
+        </form>
     </body>
 </html>
