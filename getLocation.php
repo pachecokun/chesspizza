716a33e85ebcaa378355fbc984bae42b379089f5
Saul Trujillo <strujillog1401@alumno.ipn.mx>
2016-04-24 14:48:42 -0500

    Update getLocation.php

--- getLocation.php
+++ getLocation.php
@@ -1,21 +1,29 @@
 <!DOCTYPE html>
 <html>
-<body onLoad="getLocation()">
+<body>
 
-<p id="demo">
-asdfasdf
+<p>Click the button to get your coordinates.</p>
+
+<button onclick="getLocation()">Try It</button>
+
+<p id="demo"></p>
+
 <script>
-        function getLocation(){
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-  
-    document.write("Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude);	
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;	
 }
 </script>
-</p>
-
-
 
 </body>
 </html>
-
