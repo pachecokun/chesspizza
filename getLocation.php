8ced7efdf0c88f117938fd609fdbd5aff7f04a19
Saul Trujillo <strujillog1401@alumno.ipn.mx>
2016-04-24 14:43:02 -0500

    Update getLocation.php

--- getLocation.php
+++ getLocation.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
-<body>
+<body onLoad="getLocation()">
 
 <p id="demo">
 
 <script>
+        function getLocation(){
         navigator.geolocation.getCurrentPosition(showPosition);
   
     document.write("Latitude: " + position.coords.latitude + 
