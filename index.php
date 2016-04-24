253e4bb005540f649551e2656b925d1df5e8ae8e
david <mikorreomola@gmail.com>
2016-04-23 23:02:45 -0500

    index test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head><title>Chess pizzas</title> </head>
-    <body>
+    <body style="background-color: deeppink;color: aqua">
         <h1>Chess pizza :D</h1>
         <h2>Prueba :D</h2>
     </body>
