fc40927434a808d468923a020bf1c0db8094bfa2
Saul Trujillo <strujillog1401@alumno.ipn.mx>
2016-05-08 14:07:49 -0700

    Update getLocation.php

--- getLocation.php
+++ getLocation.php
@@ -12,7 +12,7 @@ echo "Lat: " . $lat . "<br>";
 echo "Long: " . $lon . "<br><br><br>";
 $nearestSucursal = SucursalController::getNearestSucursal($lat, $lon);
 if (!is_null($nearestSucursal)) {
-    echo "Su dirección es: " . (new RouteInfo($lat,$lon,$lat,$lon))->getOriginAddress();
+    echo "Su dirección es: " . (new RouteInfo($lat,$lon,$lat,$lon))->getOriginAddress() . "\n";
     echo "La sucursal más cercana es: <br>";
     echo $nearestSucursal->getNombre() . "\n";
     echo $nearestSucursal->getDireccion();
