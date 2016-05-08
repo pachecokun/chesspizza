42a3355b2bfbbe57da0417a2a92e7cd876e168ca
Saul Trujillo <strujillog1401@alumno.ipn.mx>
2016-05-08 15:06:54 -0700

    Update SucursalController.php

--- Controller/SucursalController.php
+++ Controller/SucursalController.php
@@ -1,5 +1,5 @@
 <?php
-include_once('DAO/SucursalesDAO.php');
+include_once('DAO/SucursalDAO.php');
 include_once('RouteInfo.php');
 
 class SucursalController
@@ -7,7 +7,7 @@ class SucursalController
     public static function getNearestSucursal($lat, $lon, $maxTime = 1800) // maxTime medido en segundos
     {
         $nearestSucursal = null;
-        foreach (SucursalesDAO::getAll() as $sucursal) {
+        foreach (SucursalDAO::getAll() as $sucursal) {
             $route = new RouteInfo($lat, $lon, $sucursal->getLat(), $sucursal->getLon());
             if ($route->getResponseStatus() == "OK" && $route->getResultStatus() == "OK") {
                 if ($route->getDurationValue() < $maxTime) {
