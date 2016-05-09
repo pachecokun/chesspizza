6f62d0dd3a02efe94ebeb4fda1a992d6c220eaf9
pachecokun <mikorreomola@gmail.com>
2016-05-08 22:28:11 -0500

    agregadas funciones de dao

--- Controller/RepartidorController.php
+++ Controller/RepartidorController.php
@@ -1,6 +1,5 @@
 <?php
-
-include_once(__DIR__.'../DAO/RepartidorDAO.php');
+include_once(__DIR__.'/../DAO/RepartidorDAO.php');
 
 class RepartidorController{
     public static function registrar($nombre,$tel){
