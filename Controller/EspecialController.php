b3ba195617a8e15c66e99779712c2802ab407215
pachecokun <mikorreomola@gmail.com>
2016-05-08 23:13:10 -0500

    agregadas funciones de dao

--- Controller/EspecialController.php
+++ Controller/EspecialController.php
@@ -3,6 +3,7 @@
 include __DIR__.'/../DAO/IngredienteDAO.php';
 include __DIR__.'/../DAO/EspecialDAO.php';
 include __DIR__.'/../DAO/PizzaDAO.php';
+include __DIR__.'/IngredienteController.php';
 
 class EspecialController{
     public static function registrar(){
