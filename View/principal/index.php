863400d4e0edcbf3e3cebb48acc1d2bbb2bbed9b
DemisGM <demisgm@live.com>
2016-05-11 22:01:10 -0500

    Principal ya chida
    Me cagas sergio

--- View/principal/index.php
+++ View/principal/index.php
@@ -1,26 +1,15 @@
 <?php
-$pos = ""; //fix para la ubicación relativa en las rutas.
-$active = "inicio";
-$navElements = array(
-    "inicio" => array("INICIO", "menu", ""),
-    "ordenar" => array("ORDENAR", "menu", ""),
-    "consulta_orden" => array("CONSULTAR ORDEN", "sucursales", ""),
-    "sucursales" => array("SUCURSALES", "sucursales", ""),
-    "inicio_sesion" => array("INICIAR SESIÓN", "iniciar_sesion", "")
-);
-require_once("../htmlhead.php");
+	$pos =""; //fix para la ubicación relativa en las rutas.
+	$active = "inicio";
+	require_once($pos."../headerCliente.php");
 ?>
-    <title><?php echo $title; ?>Chess Pizza</title>
     <!-- <head> content aquí -->
 <?php
-require_once($pos."../body.php");
+	require_once($pos."../bodyCliente.php");
 ?>
-
     <!-- Contenido va aquí-->
     <img src="../img/1.jpg" style="width: 100%;">
     <img src="../img/ordena.png" style="width: 100%;">
 <?php
-include_once($pos."../footer.php");
+	include_once($pos."../footer.php");
 ?>
-Status API Training Shop Blog About
-© 2016 GitHub, Inc. Terms Privacy Security Contact Help
