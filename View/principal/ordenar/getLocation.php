aa7b0bce06a7ef21eba636ef2433e8dfae26a77c
Saul Trujillo <strujillog1401@alumno.ipn.mx>
2016-05-22 17:37:14 -0500

    Prueba de localización

---
+++ View/principal/ordenar/getLocation.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <script>    
+        function getLocation() {
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition(locate);
+            }else{
+                document.write("Este navegador no soporta la geolocalización...");
+            }
+        }
+
+        function locate(position) {
+            document.getElementById("txtLat").value=position.coords.latitude;
+            document.getElementById("txtLon").value=position.coords.longitude;
+            document.forms["formPedir"].submit();
+        }
+    </script>
+  </head>
+  <body onLoad="getLocation()">
+    Obteniendo su ubicación...    
+    <form id="formPedir" action="index.php" method="POST">
+      <input type="hidden" id="txtLat" name="Lat"/>
+      <input type="hidden" id="txtLon" name="Lon"/>    
+    </form>
+  </body>
+</html>
