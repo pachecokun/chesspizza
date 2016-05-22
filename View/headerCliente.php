e7ee5c4e738fb608c0e8efa2f9e0869e00aecefe
Saul Trujillo <strujillog1401@alumno.ipn.mx>
2016-05-22 17:49:30 -0500

    Prueba

--- View/headerCliente.php
+++ View/headerCliente.php
@@ -20,7 +20,7 @@
 	$navElements = array(
 				"inicio" => array("Inicio", "../principal", ""),
 				"carta" => array("Carta", "carta", ""),
-				"ordenar" => array("Ordenar", "../principal/ordenar/getLocation", ""),
+				"ordenar" => array("Ordenar", "/ordenar/getLocation.php", ""),
 				"miOrden" => array("Mi Orden", "miOrden", ""),
 		"sucursales" => array("Sucursales", "../Sucursal", "")
 				//"login" => array("Iniciar sesion", "login", "")
